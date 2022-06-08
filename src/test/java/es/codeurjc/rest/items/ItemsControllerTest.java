e2bd01c9d3db208bb1eee2d3669b95e42bfc30f3
Pedro Carrasco <pedrocdeveloper@gmail.com>
2022-06-08 17:59:26 +0200

    Broke test

--- src/test/java/es/codeurjc/rest/items/ItemsControllerTest.java
+++ src/test/java/es/codeurjc/rest/items/ItemsControllerTest.java
@@ -22,7 +22,7 @@ public class ItemsControllerTest {
 
         // THEN
 
-        assertThat(postedItem.getDescription()).isEqualTo(itemDescription);
+        assertThat(postedItem.getDescription()).isEqualTo(itemDescription + "a");
     }
 
 }
